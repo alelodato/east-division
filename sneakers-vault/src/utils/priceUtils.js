8a41e00bbf0e2fcd2da7380db15cc319e12f8b33
Alessio <alss.ldt@gmail.comm>
2026-03-20 10:52:09 +0100

    eliminato codice sneakers vault e collegato api keys di supabase al progetto

--- sneakers-vault/src/utils/priceUtils.js
+++
@@ -1,4 +0,0 @@
-export const hasDiscount = (p) => Number(p?.discount) > 0;
-
-export const discountedPrice = (p) =>
-  hasDiscount(p) ? Number((p.price * (1 - Number(p.discount) / 100)).toFixed(2)) : p.price;
